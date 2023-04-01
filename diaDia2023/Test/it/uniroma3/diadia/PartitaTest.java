42ab9383c771f94c3f6481091bb717c460a65fc8
GabrielMuscedere <113355330+GabrielMuscedere@users.noreply.github.com>
2023-04-01 14:19:36 +0200

    Add files via upload

---
+++ diaDia2023/Test/it/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,56 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.ambienti.Stanza;
+
+public class PartitaTest {
+	private Partita partita;
+
+	@BeforeEach
+	void setUp() {
+		this.partita = new Partita();
+	}
+	
+	@Test
+	final void testVinta() {
+		assertFalse(this.partita.vinta());
+	}
+	
+	@Test
+	final void testGetStanzaCorrente() {
+		assertNotNull(this.partita.getStanzaCorrente());
+	}
+	
+	@Test
+	final void testGetStanzaVincente() {
+		assertNotNull(this.partita.getStanzaVincente());
+	}
+
+	@Test
+	final void testIsFinita() {
+		assertFalse(this.partita.isFinita());
+		this.partita.getGiocatore().setCfu(0);
+		assertTrue(this.partita.isFinita());
+	}
+	
+	@Test
+	final void testGetGiocatore() {
+		assertNotNull(this.partita.getGiocatore());
+	}
+	
+	@Test
+	final void testGetLabirinto() {
+		assertNotNull(this.partita.getLabirinto());
+	}
+	
+	@Test
+	final void testFinita() {
+		assertFalse(this.partita.isFinita());
+	}
+	
+}
---
+++ diaDia2023/Test/it/uniroma3/diadia/ambienti/LabirintoTest.java
@@ -0,0 +1,30 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LabirintoTest {
+	private Labirinto labirinto;
+	private Stanza Biblioteca;
+	private Stanza Atrio;
+
+	@BeforeEach
+	void setUp()  {
+		this.labirinto = new Labirinto();
+		this.Biblioteca = this.labirinto.getStanzaVincente();
+		this.Atrio = this.labirinto.getStanzaIniziale();
+	}
+
+	@Test
+	void testGetStanzaVincente() {
+		assertEquals(Biblioteca, this.labirinto.getStanzaVincente());
+	}
+	
+	@Test
+	void testGetStanzaIniziale() {
+		assertEquals(Atrio, this.labirinto.getStanzaIniziale());
+	}
+
+}
